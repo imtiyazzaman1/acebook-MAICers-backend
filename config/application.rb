7ab6c4e9fdd0e2cef5125c2d1946809d2d203937
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-11-28 10:55:54 +0000

    allowed CORS

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,13 @@ module TestApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*' #-> has to be "*" or specific
+        resource '*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
@@ -31,5 +38,7 @@ module TestApp
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+
   end
 end
