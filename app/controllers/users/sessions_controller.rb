32487a701de124de3b94507e2eef88ea0c3df3e6
Muna <mmhussein125@gmail.com>
2018-11-28 11:21:55 +0000

    updates session controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,20 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
+  respond_to :json
+
+  private
+
+  def respond_with(resource, _opts = {})
+    render json: resource
+  end
+
+  def respons_to_on_destroy
+    head :no_content
+  end
+  
+end
+
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
