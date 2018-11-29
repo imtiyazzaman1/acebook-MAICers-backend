ca5c051364e3cfdfcc5e78ced194971ba3128e29
Chris Cooksley <christopher.cooksley@gmail.com>
2018-11-29 14:55:55 +0000

    create action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,13 @@ class PostsController < ApplicationController
   def index
     render json: Post.all
   end
+
+  def create
+    post = Post.create(post_params)
+    render json: post
+  end
+
+  def post_params
+    params.require(:post).permit(:message)
+  end
 end
