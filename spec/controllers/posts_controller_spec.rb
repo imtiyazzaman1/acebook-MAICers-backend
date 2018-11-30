619cafb5915772734efd3345f53d0850d17ad914
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-11-30 11:29:06 +0000

    added test for delete

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -16,4 +16,11 @@ RSpec.describe PostsController, type: :controller do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe 'DELETE /api/posts' do
+    it "responds with 200" do
+      post :destroy, params: { id: '1' }
+      expect(response).to have_http_status(204)
+    end
+  end
 end
